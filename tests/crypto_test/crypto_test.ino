feffed18c9968941e467f03c9d2e201bac632c82
chloezhu010 <chloe.zhu.x@gmail.com>
2025-09-09 14:26:23 +0200

    updated key generation & storage test

--- tests/crypto_test/crypto_test.ino
+++ tests/crypto_test/crypto_test.ino
@@ -5,58 +5,91 @@
 #include "../../source/libs/ethers/src/ethers.h" // ethereum lib
 
 void setup() {
-  /* init */
+  /* 1. init setup */
   Serial.begin(115200);
   Serial.println("Starting...");
 
-  /* init NVS flash */
+  /* 2. init NVS flash */
   if (esp_err_t err = nvs_flash_init() != ESP_OK) {
     Serial.println("NVS flash init failed");
     return;
   }
 
-  /* NVS storage setup */
+  /* 3. NVS storage setup */
   // open NVS handle
   nvs_handle_t wallet_handle;
-  // esp_err_t err;
   if (nvs_open("wallet", NVS_READWRITE, &wallet_handle) != ESP_OK) {
     Serial.println("NVS open failed");
-    // return err;
   }
   else
     Serial.println("NVS open successful");
 
   // Test NVS write/read
   uint32_t test_value = 123456;
-  if (nvs_set_u32(wallet_handle, "test_value", test_value) != ESP_OK) {
+  if (nvs_set_u32(wallet_handle, "test_value", test_value) != ESP_OK) { // store value
     Serial.println("NVS write failed");
-    // return err;
   }
   else
+  {
     Serial.println("NVS write successful");
+    nvs_commit(wallet_handle); // commit value to flash
+  }
 
   uint32_t read_value = 0;
-  if (nvs_get_u32(wallet_handle, "test_value", &read_value) != ESP_OK) {
+  if (nvs_get_u32(wallet_handle, "test_value", &read_value) != ESP_OK) { // read value
     Serial.println("NVS read failed");
-    // return err;
   }
   else
     Serial.println("NVS read successful");
   
-  if (read_value != test_value)
+  if (read_value != test_value) // check if read value is the same as the setted value
     Serial.println("❌ NVS read/write test FAILED");
   else
     Serial.println("✅ NVS read/write test PASSED");
 
-  
-    
+  /* 4. Key generation & storage*/
+  // generate test private key
+  uint8_t private_key[32];
 
-  /* Key generation & storage*/
-  // generate test secret key
+  for (int i = 0; i < 32; i++) {
+    private_key[i] = esp_random() & 0xFF; // generate private key array
+  }
+  // NEVER print the real private key
+  Serial.println("Test private key generated: "); // print private key in hex
+  for (int i = 0; i < 32; i++) {
+    Serial.print(private_key[i], HEX);
+  }
 
-  // store in NVS
+  // store pk in NVS
+  size_t private_key_size = sizeof(private_key);
+  if (nvs_set_blob(wallet_handle, "test_private_key", private_key, private_key_size) != ESP_OK) {
+    Serial.println("❌ Failed to store private key");
+  }
+  else {
+    Serial.println("✅ Private key stored successfully");
+    nvs_commit(wallet_handle); // commit value to flash
+  }
 
   // verify key retrieval
+  uint8_t retrieved_private_key[32];
+  size_t retrieved_private_key_size = sizeof(retrieved_private_key);
+  if (nvs_get_blob(wallet_handle, "test_private_key", retrieved_private_key, &retrieved_private_key_size) != ESP_OK) {
+    Serial.println("❌ Failed to retrieve private key");
+  }
+  else {
+    Serial.println("✅ Private key retrieved successfully");
+    for (int i = 0; i < 32; i++) { // print retrieved private key in hex
+      Serial.print(retrieved_private_key[i], HEX);
+    }
+    Serial.println();
+    // compare retrieved private key with original private key
+    if (memcmp(private_key, retrieved_private_key, 32) == 0) {
+      Serial.println("✅ Private key retrieved successfully");
+    }
+    else {
+      Serial.println("❌ Private key retrieved failed");
+    }
+  }
 
   /* Address derivation test */
 
@@ -65,7 +98,7 @@ void setup() {
   /* Performance & memory test */
 
   /* Cleanup*/
-  nvs_close(wallet_handle);
+  nvs_close(wallet_handle); // close NVS handle
   nvs_flash_deinit();
 
 }
